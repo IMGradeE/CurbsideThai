348d867ef15bc4024af3644316f9d0be3b7c2dd2
IMGradeE <wilkespayton@gmail.com>
2024-03-29 15:57:15 -0600

    Added bc_const.html learning activity.

--- ROOT/BridgerCollege/scripts/bc_outline.js
+++ ROOT/BridgerCollege/scripts/bc_outline.js
@@ -2,25 +2,65 @@
 
 /*
 
-   Author: 
-   Date:   
+   Author: Payton Wilkes
+   Date:   28/03/2024
 
    Filename: bc_outline.js
-
-
-   Function List
-   =============
-
-   makeOutline()
-      Generates the text of the table of contents
-      as a nested list
-
-   createList(source, TOCList, headings)
-      Creates an outline based on the source document,
-      list items are appended to TOCList,
-      the items list are based on the element names
-      specified in the headings array
-
-
 */
+window.addEventListener("load", makeOutline);
 
+let elementArray;
+
+function makeOutline() {
+    /*
+        Generates the text of the table of contents
+        as a nested list
+    */
+    const outline = document.getElementById("outline");
+    const source = document.getElementById("doc");
+    let mainHeading = document.createElement("h1");
+    let outlineList = document.createElement("ol");
+    let headingText = document.createTextNode("Outline");
+    const headings = ["H1","H2","H3","H4","H5","H6"];
+    mainHeading.appendChild(headingText);
+    outline.appendChild(mainHeading);
+    outline.appendChild(outlineList);
+    createList(source, outlineList, headings);
+}
+
+function createList(source, outlineList, headings) {
+    /*
+        Creates an outline based on the source document,
+        list items are appended to TOCList,
+        the items list are based on the element names
+        specified in the headings array
+    */
+    let lagLevel = 0;
+    let count = 0;
+    for (let sourceElement of source.childNodes) {
+        let nodeLevel = headings.indexOf(sourceElement.nodeName);
+        if (nodeLevel !== -1) {
+            let outlineNode = sourceElement.cloneNode(true);
+            sourceElement.setAttribute("id", ("head"+count));
+            if (nodeLevel > lagLevel) {
+                let ol = document.createElement("ol");
+                outlineList.lastChild.appendChild(ol);
+                outlineList = outlineList.lastChild.lastChild;
+            } else if (nodeLevel < lagLevel) {
+                let diff = lagLevel - nodeLevel;
+                for (let i = 0; i < diff; i++) {
+                    outlineList = outlineList.parentNode.parentNode;
+                }
+            }
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+            li.appendChild(a)
+            a.appendChild(outlineNode.firstChild);
+            a.setAttribute("href", ("#head" + count));
+            outlineList.appendChild(li);
+            lagLevel = nodeLevel;
+            ++count;
+        }
+    }
+
+}
--- ROOT/BridgerCollege/scripts/bc_switch.js
+++ ROOT/BridgerCollege/scripts/bc_switch.js
@@ -2,15 +2,53 @@
 
 /*
 
-   Author: 
-   Date:   
+   Author: Payton Wilkes
+   Date:   28/03/2024
 
    Filename: bc_switch.js
-   
-   setupStyles()
-   Function to set up the style sheet switcher and insert
-   from buttons to allow the user to switch between web
-   view and page view
-   
 */
+window.addEventListener("load", setupStyles)
+function setupStyles() {
+    /*
+        Function to set up the style sheet switcher and
+        insert from buttons to allow the user
+        to switch between web view and page view
+        */
+    let pageStyle = document.createElement("link");
+    pageStyle.setAttribute("href","styles/bc_page.css");
+    pageStyle.setAttribute("rel","stylesheet");
+    pageStyle.setAttribute("disabled", "true");
+    document.head.appendChild(pageStyle);
+    pageStyle.disabled=true;
 
+    let buttonDIV = document.createElement("div");
+    buttonDIV.setAttribute("id","styleButtons");
+    let pageButton = document.createElement("input");
+    pageButton.setAttribute("type","button");
+    pageButton.setAttribute("value", "page view");
+    let webButton = document.createElement("input");
+    webButton.setAttribute("type","button");
+    webButton.setAttribute("value", "web view");
+    buttonDIV.appendChild(webButton);
+    buttonDIV.appendChild(pageButton);
+    document.body.insertBefore(buttonDIV,document.body.firstChild);
+
+    let buttonStyles = document.createElement("style");
+    document.head.appendChild(buttonStyles);
+    document.styleSheets[document.styleSheets.length-1].insertRule("div#styleButtons { position: fixed;}", 0);
+
+    document.styleSheets[document.styleSheets.length-1].insertRule("div#styleButtons input{ background-color: rgba(68,94,186,0.6);" +
+        "border: 3px solid rgba(0,24,123, 0.6);" +
+        "border-radius: 50%;" +
+        "cursor:pointer;" +
+        "color:white;" +
+        "display: inline-block;" +
+        "font-size:1.2em;" +
+        "height: 60px; margin: 5px 10px; width: 100px;}", 1);
+
+    document.styleSheets[document.styleSheets.length-1].insertRule("@media print{ div#styleButtons{" +
+        "display:none;}}",2);
+
+    webButton.onclick = function (){pageStyle.disabled = true;};
+    pageButton.onclick = function (){pageStyle.disabled = false;};
+}
