9aeb9eaf7bc1212c97563cc9b44d4fc2bb44a4c4
IMGradeE <wilkespayton@gmail.com>
2024-03-30 16:16:13 -0600

    Added petcompetition.js and associated html for web form handling challenge activity.

--- ROOT/6-7Starter/scripts/petcompetition.js
+++ ROOT/6-7Starter/scripts/petcompetition.js
@@ -1,15 +1,58 @@
 "use strict"
-window.addEventListener("load", init);
 
-function set
+/*Easier than passing arguments and acceptable in this context*/
+const checkBoxes = [
+    document.getElementById("trick"),
+    document.getElementById("sing"),
+    document.getElementById("cute"), ];
+const sizeElement = document.getElementById("size");
+const daysElement = document.getElementById("days");
+const weightElement= document.getElementById("weight");
+const boardingFeeElement = document.getElementById("boardingFee");
+
+const totalCostEle = document.getElementById("totalCost");
+const boardingCostEle = document.getElementById("boardingCost");
+const registrationCostEle = document.getElementById("registrationCost");
+
+const boardingFee = 19.99;
+
+let numberOfEvents = 0;
+
+window.addEventListener("load",         function () {
+    document.styleSheets[document.styleSheets.length-1].insertRule('div[id$="Add"]{display:none;}');
+    for (let checkBox of checkBoxes){
+        checkBox.setAttribute("name", "null");
+    }
+    init();
+    });
+
+function fString(x){
+    return x.toFixed(2);
+}
+
 function init() {
-    const boardingFee = 19.99;
-    const sizeElement = document.getElementById("size");
     let size;
-    let weightElement= document.getElementById("weight");
     let weight = weightElement.value;
-
-    weightElement.addEventListener("focusout", function (){
+    /*The fact that checkboxes do not have a native indicator of their state is appalling, but it's a great example of
+    * institutional inflexibility's relationship to technological determinism */
+    for (let checkBox of checkBoxes) {
+        checkBox.addEventListener("click", function (e) {
+            /*Unchecked -> checked*/
+            if (e.target.name === "null") {
+                ++numberOfEvents;
+                e.target.setAttribute("name", e.target.id);
+                document.getElementById(e.target.id+"Add").style.display = "block";
+            }
+            /*Checked -> Unchecked*/
+            else{
+                --numberOfEvents;
+                e.target.setAttribute("name",  "null");
+                document.getElementById(e.target.id+"Add").style.display = "none";
+            }
+            totalcosts();
+        } );
+    }
+    weightElement.addEventListener("input", function (){
         weight = weightElement.value;
         if(!isNaN(weight)){
             if (weight <= 4) {
@@ -27,66 +70,34 @@ function init() {
         sizeElement.value = size;
     });
 
-    let daysElement = document.getElementById("days");
-    let boardingFeeElement = document.getElementById("boardingFee");
     let days = daysElement.value;
     let cost;
-    daysElement.addEventListener("focusout", function (){
+    daysElement.addEventListener("input", function (){
         days = parseInt(daysElement.value,10);
         if(!isNaN(days)){
             daysElement.value = (days);
             cost = days*boardingFee;
-            boardingFeeElement.value = cost.toFixed(2);
+            boardingFeeElement.value = fString(cost);
         }else{
             daysElement.value =("0");
             boardingFeeElement.value =("0.00");
         }
+        totalcosts();
     });
+
 }
 
-
-
-function totalcosts(boardingCost){
+function totalcosts(){
+    const costPerEvent = 120;
     let registrationCost = 0;
-    let numberOfEvents = 0;
-    if (isNaN(boardingCost)) boardingCost = 0;
-
-    /*If the sing checkbox is checked *check below for an example*/
-    /*add one to the number of events*/
-    /*If the cute checkbox is checked*/
-    /*add one to the number of events*/
-    /*If the trick checkbox is checked*/
-    /*add one to the number of events*/
-    /*Compute the registration cost*/
-    /*it is 120 * the number of events*/
-    /*Compute the total*/
-    /*it is the boarding cost + registration cost*/
-    /*Display the boarding cost in the boardingCost element*/
-    /*specify this value only has two digits after the decimal place*/
-    /*Display the registration cost in the registrationCost element*/
-    /*specify this value only has two digits after the decimal place*/
-    /*Display the total in the totalCost element*/
-    /*specify this value only has two digits after the decimal place*/
-    /*Now go back to the days of boarding event handler*/
-    /*add a call to this update total cost function*/
-    /*test it to make sure the totals update as expected*/
-
+    let totalCost = 0;
+    let boardingCost = parseInt(boardingFeeElement.value);
+    if (isNaN(boardingCost)){
+        boardingCost = 0;
+    }
+    registrationCost = (costPerEvent*numberOfEvents)
+    totalCost = boardingCost + registrationCost;
+    registrationCostEle.value = fString(registrationCost);
+    boardingCostEle.value = fString(boardingCost);
+    totalCostEle.value = fString(totalCost);
 }
-
-/*Add checkbox event handlers
-* There are three checkboxes. Each is followed by a div containing a fieldset, labels, and input elements. This div is to only be shown when the associated checkbox is checked.*/
-
-/*In css file
-* Add a style block for these three divs
-Set the display property to none*/
-
-/*Add event handler to the sing checkbox
-* Have it respond to the change event
-If the checkbox is checked
-    set the display to block
-If the checkbox is not checked (can just use else)
-    set the display to none
-add a call to the update total cost function
-test it to make sure the totals update as expected*/
-
-/*Add a similar event listener to the cute and trick checkboxes*/
