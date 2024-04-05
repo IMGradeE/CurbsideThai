310b689dd20c21ea3fabe1f4832367c4d66891f7
IMGradeE <wilkespayton@gmail.com>
2024-04-04 22:04:44 -0600

    Finished final, still needs entry to be finished on assignments page.

--- ROOT/Final/javascript/game.js
+++ ROOT/Final/javascript/game.js
@@ -9,34 +9,76 @@ button: startButton
 //
 window.addEventListener("load", init);
 
+let gameFlipped = 0;
+let gameCount = 0;
+let lifetimeFlipped = 0;
+let flippedCardPair = [];
+let pairs;
+
 function init (){
     const startForm = document.getElementById("startForm");
     const game = document.getElementById("game");
     const head = document.head;
+    const startButton =  document.getElementById("startButton");
+    head.getElementsByTagName("title")[0].innerHTML = "Match Game";
+    document.body.children[0].setAttribute("id", "titleH1");
+    let titleH1 = document.getElementById("titleH1");
+    titleH1.style.float = "left";
+    titleH1.style.marginLeft = "10%";
+    // add row displaying stats (got lazy)
+    let statsRow = document.createElement("header");
+    statsRow.setAttribute("id", "stats");
+
+    statsRow.innerHTML ='<div class="gridNoDecor"><div class="col_1-1" id="colheader"><fieldset class="outerFieldset"><div class="col_1-10"><a href="#"><img src="/Final/images/logo.png"></a></div><div class="col_9-10" id="colheaderinner"><h1 id = "titleH1Here"></h1><a class="headerAnchor" href="#"><h1 id = "gamesCount"></h1></a><a class="headerAnchor" href="#"><h1 id ="lifetimeFlipped"></h1></a></div></div></fieldset></div>';
+    document.body.insertBefore(statsRow,game);
+
+    document.getElementById("titleH1Here").replaceWith(titleH1);
+    titleH1.style.fontStyle = "italic";
+    titleH1.style.textAlign = "left";
+    titleH1.style.margin = "0";
+    titleH1.style.fontSize = "xxx-large";
+    let localHistoryFlipped = localStorage.getItem("flips");
+    let localHistoryGames = localStorage.getItem("wins");
+    const storedFlips = parseInt(localHistoryFlipped);
+    const storedGames = parseInt(localHistoryGames);
+
+    if (!isNaN(storedFlips)){
+        lifetimeFlipped += storedFlips;
+    }
+    if (!isNaN(storedGames)) {
+        gameCount += storedGames;
+    }
+
+    document.getElementById("lifetimeFlipped").innerHTML = "Lifetime Guesses: " + Math.floor(lifetimeFlipped/2);
+
+    document.getElementById("gamesCount").innerHTML = "Games Played: " + gameCount;
 
     // add class to startform div and game div
+
     game.setAttribute("class","outerFieldset");
-    startForm.setAttribute("class","grid");
+    startForm.setAttribute("class","row");
+    startForm.style.fontSize = "x-large";
+    startForm.style.textAlign = "center";
 
-    //Track wins/losses, cards flipped.
-    let sessionWins = 0;
-    let sessionLosses = 0;
-    let sessionFlipped = 0;
+    startButton.insertAdjacentHTML("beforebegin", "<br>");
+    startButton.style.backgroundColor = "transparent";
+    startButton.style.fontSize = "medium";
+    startButton.style.border = "2px aliceblue ridge";
+    startButton.style.borderRadius = "8px";
+    startButton.style.color = "darkslategray";
+    startButton.style.width = "100%";
+    startButton.style.boxShadow = "inset 0 0 5px 10px rgba(243, 251, 255, 0.5)";
+    startForm.style.padding = ".5em 0 0 0";
 
-    // TODO if any of these const's are defined, "welcome back", else nothing
-    const previousWins = localStorage.getItem("wins")
-    sessionWins += parseInt(previousWins);
-    const previousLosses = localStorage.getItem("losses");
-    sessionLosses += parseInt(previousLosses);
-    const previousFlipped = localStorage.getItem("flipped")
-    sessionFlipped += parseInt(previousFlipped);
+
     let symbolPairs = 0;
 
     // set max symbols to 8, min to 3,
     let numSymbols = document.getElementById("numSymbols");
+    numSymbols.nextSibling.replaceWith(document.createTextNode("Symbols."));
     numSymbols.setAttribute("max", "8");
-    numSymbols.setAttribute("min", "3"); // Otherwise what's the point?
-    numSymbols.setAttribute("value", "3");
+    numSymbols.setAttribute("min", "2"); // Otherwise what's the point?
+    numSymbols.setAttribute("value", "2");
 
     // add stylesheet
     let globalSheet = document.createElement("link");
@@ -54,81 +96,203 @@ function init (){
     sheet.innerHTML += ("div{} "/*TODO*/);
 
     /* IDEAS FOR STYLES
-    * CSS animation transitions from one color to another by using a linear gradient*/
-
-    // add event listener that saves wins/losses
-    window.addEventListener("beforeunload", beforeUnload(sessionWins,sessionLosses,sessionFlipped))
-
-    document.getElementById("startButton").addEventListener("click", function(){
-        symbolPairs = parseInt(numSymbols.value)*2;
-        // TODO generate symbols dynamically
-        //Hide everything in div#startform
+    * CSS animation transitions from one color to another by using a linear gradient (didn't do this, but it's a nice thought)*/
+   startButton.addEventListener("click", function(e){
         startForm.style.display = "none";
-        // run Game (returns a list of [bool, cardsFlipped]
-        // TODO pass aray as argument instead of number of cards
-        let resultJSON = gameFunc(symbolPairs, game);
-        if ( resultJSON.win === true){
-            ++sessionWins
-        }else{
-            ++sessionLosses
-        }
-        sessionFlipped += resultJSON.flipped;
+        e.preventDefault();
+        pairs = parseInt(numSymbols.value);
+        symbolPairs = pairs*2;
+        let symbolArr = [];
+        symbolArr = getSymbols(symbolArr);
+        displayCards(symbolArr, game);
     });
-
-
 }
 
-// returns win:bool, flipped:int(number), flipped and win are counted in init.
-function gameFunc(/*pairs, */nCards,game){
-    /*let nCards = pairs.length;*/
-    let pSquares = [4,9,16];
-    let nRows=0, nCols=0;
-    let colClass;
-    // Insert (pairs) divs
-    // Math.sqrt(nPairs).toFixed(0) === Math.sqrt(nPairs)
-    // This condition is overpowered given the range of inputs only contains 3 perfect squares but I still like it
+function getSymbols(symbolArr){
+    while (symbolArr.length < pairs) {
+        let symbolString = String.fromCharCode(33+(Math.floor(Math.random()*100)%33));
+
+        if(!symbolArr.includes(symbolString)){
+            symbolArr.push(symbolString);
+        }
+    }
+    //duplicate every item
+    const preLen = symbolArr.length;
+    for (let i = 0; i < preLen; ++i) {
+        symbolArr.push(symbolArr[i]);
+    }
+    return shuffle(symbolArr, pairs*2);
+}
+
+function shuffle(symbolArr, symbolPairs){
+    let temp;
+    for (let i = 0; i < 16; ++i){
+        let index1 = Math.floor(Math.random()*100)%symbolPairs;
+        let index2 = i%symbolPairs;
+        temp = symbolArr[index1];
+        symbolArr[index1] = symbolArr[index2];
+        symbolArr[index2] = temp;
+    }
+    return symbolArr;
+}
+
+function gameFunc(){
+    let game = document.getElementById("game");
+    if (pairs === 0){
+        while(game.children.length > 1)  {
+            let element = game.children[1];
+            element.remove();
+        }
+        ++gameCount;
+        lifetimeFlipped += gameFlipped;
+        gameFlipped = 0;
+
+        storeLocally(gameCount, lifetimeFlipped);
+        // this should be part of the other function that updates stats but I don't have time to refactor every redundant piece of code.
+        document.getElementById("lifetimeFlipped").innerHTML = "Lifetime Guesses: " + Math.floor(lifetimeFlipped/2);
+        document.getElementById("gamesCount").innerHTML = "Games Played: " + gameCount;
+        document.getElementById("startForm").style.display = "block";
+        document.getElementById("stats").style.display = "block";
+    }// mock return value
+}
+
+function changeGuesses(gameFlipped) {
+    let flippedCount = document.getElementById("flippedDisplay");
+    flippedCount.innerHTML = "Guesses: "+ Math.floor(gameFlipped/2);
+}
+
+function displayCards(symbolArr, game){
+    document.getElementById("stats").style.display = "none";
+    let nCards = symbolArr.length;
+    let flipped;
+    let pSquares = [4,16];
+    let nRows, nCols, colClass;
+    let flippedCardsDiv = document.createElement("div");
+
+    flippedCardsDiv.setAttribute("class", "row");
+    flippedCardsDiv.setAttribute("id", "flippedDisplay");
+    flippedCardsDiv.style.fontSize ="xx-large";
+    game.appendChild(flippedCardsDiv);
+    changeGuesses(gameFlipped);
+    // Math.sqrt(nPairs).toFixed(0) === Math.sqrt(nPairs) This condition is unnecessary given the range of inputs only contains 3 perfect squares, and one of them is unattainable, but I think it's cool.
     if(pSquares.includes(nCards)){
         nRows = Math.sqrt(nCards);
         nCols = nRows;
         colClass = "col_"+(100/nCols);
     }else{
-        nRows = 4;
-        nCols = nCards/4;
-        colClass = "col_25";
+        nRows = Math.ceil(nCards/3);
+        nCols = 3;
+        colClass = "col_33";
     }
-    /*TODO add alt behavior*/
 
-    // should probably move this to a helper
+    const root = document.getElementsByTagName("html")[0];
+    console.log(root)
+    console.log(parseInt(root.clientHeight))
+    const height = parseInt(root.clientHeight)/nRows - 50;
 
+    let count = 0;
     for (let i = 0; i < nRows; ++i){
         let rowDiv = document.createElement("div");
-        for (let j = 0; j < nCols; ++j){
-            let cardID = i+j;
+        for (let j = 0; j < nCols && nCards !== 0; ++j){
+            let cardID = count;
             let cardIDString = "card_"+cardID;
-            //This is a container for the symbol cards,
-            // I still need to insert the actual cards.
-            /*TODO insert symbols*/
+
             let colDiv = document.createElement("div");
-            let cardDiv = document.createElement("div");
-            cardDiv.setAttribute("class", "gridEntry");
+            let currentSymbol = symbolArr[cardID]; // TODO add chars
+            let cardSymbol = document.createTextNode(currentSymbol);
+
+            let attributeArray = [
+                {name: "id", value: cardIDString},
+                {name: "class", value:"gridEntry"},
+                {name: "value", value: currentSymbol}];
+
+            let backOfCardDiv = getDivWithAttributes(attributeArray, cardSymbol, height);
+            // add ID suffix
+            attributeArray[0].value = attributeArray[0].value +"f";
+            let faceOfCardDiv = getDivWithAttributes(attributeArray, cardSymbol, height);
+
             colDiv.setAttribute("class", colClass);
-            cardDiv.setAttribute("id", cardIDString);
-            cardDiv.addEventListener("click", function (){
-                cardDiv.style.display = "none";
-            });
-            cardDiv.innerHTML = "@";
-            colDiv.appendChild(cardDiv);
+            // append cards; back, then face.
+            colDiv.appendChild(backOfCardDiv);
+            colDiv.appendChild(faceOfCardDiv);
+            //append row of cards
             rowDiv.appendChild(colDiv);
+            // decrement control variable
+            --nCards;
+            ++count;
         }
-        document.getElementById("game").appendChild(rowDiv);
+        game.appendChild(rowDiv);
     }
-    return {win: true, flipped: 5};
+    for (let div of document.querySelectorAll("div.gridEntry")){
+
+    }
 }
 
+function getDivWithAttributes(attributes, cardSymbol, height) {
+    let funcDiv = document.createElement("div");
+    for (let attribute of attributes) {
+        funcDiv.setAttribute(attribute.name, attribute.value);
+        if (attribute.name === 'id' && attribute.value.endsWith('f')){
+            funcDiv.style.display = "none";
+        }else if (attribute.name === 'id' && !attribute.value.endsWith('f')){
+            funcDiv.style.display = "block";
+            funcDiv.addEventListener("click", flipCard);
+        }
+    }
+    funcDiv.style.fontSize = "xxx-large";
+    console.log(height);
+    funcDiv.style.minHeight = height.toString()+"px";
+    funcDiv.appendChild(cardSymbol);
+    return funcDiv;
+}
+
+function flipCard(e){
+    let cardBack = e.target;
+    let cardFace = cardBack.nextSibling;
+    ++gameFlipped;
+
+    changeGuesses(gameFlipped);
+    cardBack.style.display = "none";
+    cardFace.style.display = "block";
+    flippedCardPair.push(({back: cardBack, face:cardFace})) // not ideal solution but I don't want to refactor...
+    if (gameFlipped % 2 === 0){
+        // don't allow any more clicks
+        let allCards = document.querySelectorAll("div[id^='card_']");
+        for (let card of allCards) {
+            if (!card.id.endsWith('f')) {
+                card.removeEventListener("click", flipCard);
+            }
+        }
+        setTimeout(function (){
+            if(flippedCardPair[0].back.getAttribute("value") === flippedCardPair[1].back.getAttribute("value")){
+                cardFace.style.boxShadow = "inset 0 0 20px 10px lightgreen";
+                flippedCardPair[0].face.style.boxShadow ="inset 0 0 20px 10px lightgreen";
+                --pairs;
+            }else{
+                cardBack.style.display = "block";
+                cardFace.style.display = "none";
+                flippedCardPair[0].back.style.display = "block";
+                flippedCardPair[0].face.style.display = "none";
+            }
+            flippedCardPair = [];
+            // allow more clicks
+            for (let card of allCards) {
+                if (!card.id.endsWith('f')) {
+                    card.addEventListener("click", flipCard);
+                }
+            }
+            // checks if the player has won
+            gameFunc();
+        }, 500);
+
+    }
+
+}
+
+
 // function sets all localstorage https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-function beforeUnload( wins, losses, flips) {
-    localStorage.getItem("wins", wins);
-    localStorage.getItem("losses", losses);
-    localStorage.getItem("flips", flips);
+function storeLocally( wins, flips) {
+    localStorage.setItem("wins", wins);
+    localStorage.setItem("flips", flips);
 }
 
